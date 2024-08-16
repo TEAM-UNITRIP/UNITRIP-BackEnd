7e0ac08e91d5da5a6851feaeec02878a78394570
aazkgh <gg25hp17@naver.com>
2024-08-17 03:00:17 +0900

    init: eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import'; 
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,41 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      "indent": ["error", 2],
+      "quotes": ["off", "single"],
+      "semi": ["error", "always"],
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+      "no-multi-spaces": "error",
+      "comma-dangle": ["error", "always-multiline"], 
+      "object-curly-spacing": ["error", "always"], 
+      "space-in-parens": ["error", "never"],
+      "computed-property-spacing": ["error", "never"], 
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "off", // Checks effect dependencies
+      "react/react-in-jsx-scope": "off", 
+      "no-unused-vars": "off",
+      "no-console": "warn",
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "parent", "sibling", "index"], // import 되는 순서 정의
+          "pathGroups": [
+            {
+              "pattern": "react*", // path가 react로 시작하면
+              "group": "external", // external 앞에
+              "position": "before",
+            },
+          ],
+          "alphabetize": {
+            //group 내부에서 알파벳 오름차순으로 정렬
+            "order": "asc",
+            "caseInsensitive": true,
+          },
+          // group들 사이마다 개행 적용 (group 내부에서 개행 적용 불가)
+          "newlines-between": "always", 
+        },
+      ],
     },
   },
-]
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 const supabase = createClient("https://hxwydpaqcdilnqahvmjf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4d3lkcGFxY2RpbG5xYWh2bWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2MTM4MjEsImV4cCI6MjAzOTE4OTgyMX0.gHDIyM_ix4FDx3aTyRBlVEqLjaQ097iLbGRAbH3OWr4");
 
@@ -13,7 +13,6 @@ function App() {
   async function getUsers() {
     const { data } = await supabase.from("USER").select();
     setUsers(data);
-    console.log(data)
   }
 
   return (
